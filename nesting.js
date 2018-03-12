cc9842005ec161d4b76a63b4969ebb1e6b2624b3
Benjamin Alldredge <benjaminalldredge@Benjamins-MacBook-Pro.local>
2018-03-12 11:39:41 -0600

    finished js 3 afternoon project

--- nesting.js
+++ nesting.js
@@ -51,7 +51,16 @@ var employees = [
 */
 
 //Code Here
-
+function employeeUpdater() {
+  for(var i=0; i<employees.length; i++) {
+    if (employees[i].firstName === 'Theo') {
+      employees.splice(i, 1);
+    } else if (employees[i].firstName === 'Lorie') {
+      employees[i].department = 'HR';
+    }
+  }
+  return employees;
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -69,7 +78,16 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 */
 
 //Code Here
-
+function removeDuplicates() {
+  let newWorkplaceAccidents = [];
+  for (var i=0; i<workplaceAccidents.length; i++) {
+    if(newWorkplaceAccidents.indexOf(workplaceAccidents[i]) === -1) {
+      newWorkplaceAccidents.push(workplaceAccidents[i]);
+      }
+    }
+    workplaceAccidents = newWorkplaceAccidents;
+    return workplaceAccidents;
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -97,8 +115,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 
@@ -139,7 +157,11 @@ var myCar = {
 */
 
 //Code Here
-
+function recordCleaner() {
+  for(var i=0; i<myCar.accidents.length; i++) {
+    myCar.accidents[i].atFaultForAccident = false;
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -158,5 +180,16 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
 */
 
 //Code Here
-
+function looper() {
+  for(var i=0; i<numsArr.length; i++) {
+    for(var j=0; j<numsArr[i].length; j++) {
+      if(numsArr[i][j] % 2 === 0) {
+        numsArr[i][j] = 'even'
+      } else {
+        numsArr[i][j] = 'odd'
+      }
+    }
+  }
+  return numsArr;
+}
 
